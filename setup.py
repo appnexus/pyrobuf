f6cfcf184e814f80bae4c629ceb42ede1d398af3
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2016-01-08 23:34:14 +1100

    setup.py foundation

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
+import os, sys
 from distutils.core import setup
+from distutils.command.install import install as _install
+
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize(["src/*.pyx", "out/*_proto.pyx"],
-                            include_path=["out", "src"]))
+
+VERSION = "0.0.1"
+
+def _post_install():
+    setup(name="pyrobuf_postinstall",
+          version=VERSION,
+          ext_modules=cythonize(["src/*.pyx"],
+                                include_path=["src"]),
+          script_args=['build', 'install'])
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, (), msg="Running post install task")
+
+setup(
+    name="pyrobuf",
+    version=VERSION,
+    cmdclass={'install': install},
+)
