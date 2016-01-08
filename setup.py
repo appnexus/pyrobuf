51c52156e17d331489317d87294cf9c68fcd666b
Eugene Van den Bulke <eugvan@optiver.com>
2016-01-08 14:55:56 +1100

    alignment

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(ext_modules=cythonize(["src/*.pyx", "out/*_proto.pyx"],
-    include_path=["out", "src"]))
+                            include_path=["out", "src"]))
