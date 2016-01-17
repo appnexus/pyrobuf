2d7830327fde76bcbce530c91d8d7ac7e33f5b7c
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2016-01-17 11:39:51 +0800

    HERE in setup

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ from Cython.Build import cythonize
 
 
 VERSION = "0.0.1"
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 def _post_install():
     setup(name="pyrobuf_postinstall",
           version=VERSION,
-          ext_modules=cythonize(["src/*.pyx"],
-                                include_path=["src"]),
+          ext_modules=cythonize([os.path.join(HERE, 'pyrobuf', 'src', '*.pyx')],
+                                include_path=[os.path.join(HERE, 'pyrobuf', 'src')]),
           script_args=['build', 'install'])
 
 class install(_install):
