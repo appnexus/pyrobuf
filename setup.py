6f60035d65f8550e9d12977bb391fbd6137159f8
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2016-01-25 22:38:20 +1100

    setup.py list_and_util command

--- setup.py
+++ setup.py
@@ -27,20 +27,35 @@ class GenerateList(Command):
         assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
         self.execute(_pre_install, (self, ), msg="Running pre install task")
 
+class ListAndUtil(Command):
+
+    description = "compile and install pyrobuf_list and pyrobuf_util (for development)"
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        self.execute(_post_install, (self, ), msg="Running post install task")
+
 class install(_install):
 
     def run(self):
         self.execute(_pre_install, (self, ), msg="Running pre install task")
         _install.run(self)
-        self.execute(self._post_install, (), msg="Running post install task")
+        self.execute(_post_install, (self, ), msg="Running post install task")
 
-    def _post_install(self):
-        self.announce('Building & installing pyrobuf_util and pyrobuf_list', level=distutils.log.INFO)
-        setup(name="pyrobuf_postinstall",
-              version=VERSION,
-              ext_modules=cythonize([os.path.join(HERE, 'pyrobuf', 'src', '*.pyx')],
-                                    include_path=[os.path.join(HERE, 'pyrobuf', 'src')]),
-              script_args=['build', 'install'])
+def _post_install(command):
+    command.announce('Building & installing pyrobuf_util and pyrobuf_list', level=distutils.log.INFO)
+    setup(name="pyrobuf_list_and_util",
+          version=VERSION,
+          ext_modules=cythonize([os.path.join(HERE, 'pyrobuf', 'src', '*.pyx')],
+                                include_path=[os.path.join(HERE, 'pyrobuf', 'src')]),
+          script_args=['build', 'install'])
 
 def _pre_install(command):
     env = Environment(loader=PackageLoader('pyrobuf.protobuf', 'templates'))
@@ -78,7 +93,9 @@ setup(
     version=VERSION,
     packages=find_packages(),
     include_package_data=True,
-    cmdclass={'install': install, 'generate_list': GenerateList },
+    cmdclass={'install': install,
+              'generate_list': GenerateList,
+              'list_and_util': ListAndUtil},
     entry_points={
         'console_scripts': ['pyrobuf = pyrobuf.__main__:main']
     },
