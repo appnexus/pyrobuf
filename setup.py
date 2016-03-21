037ecbd20f136aa327b69d8d82df25b8b4675e3b
Tin Tvrtkovic <tinchester@gmail.com>
2016-03-21 23:14:02 +0100

    Single distribution setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-import os
-import sys
-import distutils.log
-
 from setuptools import setup, find_packages, Command
 from setuptools.command.install import install as _install
+
+import os
+import sys
 
 
 VERSION = "0.5.5"
@@ -25,6 +24,7 @@ class GenerateList(Command):
         assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
         self.execute(_pre_install, (self, ), msg="Running pre install task")
 
+
 class ListAndUtil(Command):
 
     description = "compile and install pyrobuf_list and pyrobuf_util (for development)"
@@ -40,33 +40,26 @@ class ListAndUtil(Command):
         assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
         self.execute(_post_install, (self, ), msg="Running post install task")
 
+
 class install(_install):
-
     def run(self):
-        self.execute(_pre_install, (self, ), msg="Running pre install task")
-        _install.run(self)
-        self.execute(_post_install, (self, ), msg="Running post install task")
+        # By now the setup_requires deps have been fetched.
+        self.distribution.ext_modules = self.pyrobufize_builtins()
+        super(_install, self).run()
 
-def _post_install(command):
-    from Cython.Build import cythonize
-    command.announce('Building & installing pyrobuf_util and pyrobuf_list', level=distutils.log.INFO)
-    setup(name="pyrobuf_list_and_util",
-          version=VERSION,
-          ext_modules=cythonize([os.path.join(HERE, 'pyrobuf', 'src', '*.pyx')],
-                                include_path=[os.path.join(HERE, 'pyrobuf', 'src')]),
-          script_args=['build', 'install'])
+    @staticmethod
+    def pyrobufize_builtins():
+        from jinja2 import Environment, PackageLoader
+        from Cython.Build import cythonize
+        env = Environment(loader=PackageLoader('pyrobuf.protobuf', 'templates'))
 
-def _pre_install(command):
-    from jinja2 import Environment, PackageLoader
-    env = Environment(loader=PackageLoader('pyrobuf.protobuf', 'templates'))
+        name_pyx = 'pyrobuf_list.pyx'
+        name_pxd = 'pyrobuf_list.pxd'
 
-    name_pyx = 'pyrobuf_list.pyx'
-    name_pxd = 'pyrobuf_list.pxd'
+        templ_pyx = env.get_template('pyrobuf_list_pyx.tmpl')
+        templ_pxd = env.get_template('pyrobuf_list_pxd.tmpl')
 
-    templ_pyx = env.get_template('pyrobuf_list_pyx.tmpl')
-    templ_pxd = env.get_template('pyrobuf_list_pxd.tmpl')
-
-    listdict = {
+        listdict = {
             'DoubleList':   'double',
             'FloatList':    'float',
             'IntList':      'int',
@@ -75,17 +68,18 @@ def _pre_install(command):
             'Int64List':    'int64_t',
             'Uint64List':   'uint64_t',
             'CharList':     'char'
-    }
+        }
 
-    path = os.path.join(HERE, 'pyrobuf', 'src', name_pyx)
-    command.announce('    Generating {0}'.format(path), level=distutils.log.INFO)
-    with open(path, 'w') as fp:
-        fp.write(templ_pyx.render({'def': listdict, 'version_major': sys.version_info.major}))
+        path = os.path.join(HERE, 'pyrobuf', 'src', name_pyx)
+        with open(path, 'w') as fp:
+            fp.write(templ_pyx.render({'def': listdict, 'version_major': sys.version_info.major}))
 
-    path = os.path.join(HERE, 'pyrobuf', 'src', name_pxd)
-    command.announce('    Generating {0}'.format(path), level=distutils.log.INFO)
-    with open(path, 'w') as fp:
-        fp.write(templ_pxd.render({'def': listdict, 'version_major': sys.version_info.major}))
+        path = os.path.join(HERE, 'pyrobuf', 'src', name_pxd)
+        with open(path, 'w') as fp:
+            fp.write(templ_pxd.render({'def': listdict, 'version_major': sys.version_info.major}))
+
+        return cythonize(['pyrobuf/src/*.pyx'],
+                         include_path=['pyrobuf/src'])
 
 
 setup(
@@ -97,7 +91,7 @@ setup(
               'generate_list': GenerateList,
               'list_and_util': ListAndUtil},
     entry_points={
-        'console_scripts': ['pyrobuf = pyrobuf.__main__:main']
+        'console_scripts': ['pyrobuf = pyrobuf.__main__:main'],
     },
     description='A Cython based protobuf compiler',
     long_description=open(os.path.join(HERE, 'README.md')).read(),
