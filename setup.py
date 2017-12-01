5765e2bd6aefc9639bfd9c18d8544f87f0afdba7
Taylor Burmeister <tburmeiste@appnexus.com>
2017-12-01 17:19:17 -0500

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import platform
 import sys
 
 
-VERSION = "0.6.2"
+VERSION = "0.6.3"
 HERE = os.path.dirname(os.path.abspath(__file__))
 PYROBUF_DEFS_PXI = "pyrobuf_defs.pxi"
 PYROBUF_LIST_PXD = "pyrobuf_list.pxd"
