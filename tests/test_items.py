942db43415e6a9dc414c412559f69dff56a8de24
Taylor Burmeister <burmeister.taylor@gmail.com>
2018-03-12 09:44:21 -0400

    add a Setters method that yields setters for each field in a message … (#100)
    * add methods Items, Fields, Values to allow user to iterator over fields, values, and field/value pairs of a message
    
    * fix indentation
    
    * add a Setters method that yields setters for each field in a message and moved from deprecated Cython property to property decorator
    
    * added a test for Setters method and in the process fixed a bug in scalar list assignment and added a sane equality method for scalar lists

--- tests/test_items.py
+++ tests/test_items.py
@@ -5,6 +5,11 @@ from create_message import create_an_test
 
 class ItemsTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        global Test
+        from test_message_proto import Test
+
     def test_fields(self):
         test = create_an_test()
         fields = [
@@ -26,3 +31,13 @@ class ItemsTest(unittest.TestCase):
         test = create_an_test()
         for field, value in test.Items():
             self.assertEqual(value, getattr(test, field))
+
+    def test_setters(self):
+        source = create_an_test()
+        dest = Test()
+
+        for setter, value in zip(dest.Setters(), source.Values()):
+            setter(value)
+
+        for source_val, dest_val in zip(source.Values(), dest.Values()):
+            self.assertEqual(source_val, dest_val)
