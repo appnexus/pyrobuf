1aaee3c6c5c6b3b9ea7cbc3da3cac8a08e7ea0e6
Laurynas Alekna <laurynas@alekna.org>
2020-01-31 18:39:59 +0000

    fix broken setuptools_ext

--- pyrobuf/setuptools_ext.py
+++ pyrobuf/setuptools_ext.py
@@ -1,17 +1,24 @@
 """Setuptools integration."""
+import os
+import sys
+
 from pyrobuf.compile import Compiler
 
-try:
-    basestring
-except NameError:
-    # Python 3.x
+if sys.version_info.major == 3:
     basestring = str
+    _FileExistsError = FileExistsError
+else:
+    _FileExistsError = OSError
 
 
 def add_pyrobuf_module(dist, pyrobuf_module):
     dir_name = "pyrobuf/_" + pyrobuf_module
-    compiler = Compiler(pyrobuf_module, out=dir_name,
-                        package="{}_{}".format(dist.get_name(), pyrobuf_module))
+    package = "{}_{}".format(dist.get_name(), pyrobuf_module)
+    try:
+        os.makedirs(os.path.join(dir_name, package))
+    except _FileExistsError:
+        pass
+    compiler = Compiler([pyrobuf_module], out=dir_name, package=package)
     compiler.extend(dist)
 
 
